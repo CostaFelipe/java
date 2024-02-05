35aa01cb8bdbe7d8533297d48853e39ee63eff08
wedo <costadefelipe@gmail.com>
2024-02-05 20:15:50 -0300

    algoritmo para imprimir números pares de uma lista

--- array/L1Ex20Loops.java
+++ array/L1Ex20Loops.java
@@ -2,6 +2,18 @@ package treinando.devdojo.maratonajava.exercicio.estruturas;
 
 public class L1Ex20Loops {
     public static void main(String[] args) {
+        Even even = new Even();
+        int[] lista = new int[]{1,2,33,4,56, 101, 999, 1002};
+        even.numberEven(lista);
+    }
+}
 
+class Even {
+    public void numberEven(int[] n){
+        for (int i = 0; i < n.length; i++){
+            if(n[i] % 2 == 0){
+                System.out.print(" " + n[i]);
+            }
+        }
     }
 }
