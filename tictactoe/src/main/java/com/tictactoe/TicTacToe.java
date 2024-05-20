1fb6ef997ece016cbd535a2a8296a155478287bf
wedo <costadefelipe@gmail.com>
2024-05-20 13:35:28 -0300

    create method putMark, isWin, winner

--- tictactoe/src/main/java/com/tictactoe/TicTacToe.java
+++ tictactoe/src/main/java/com/tictactoe/TicTacToe.java
@@ -18,4 +18,38 @@ public class TicTacToe {
     }
     player = X;
   }
+
+  public void putMark(int i, int j) throws IllegalAccessException {
+    if ((i < 0) || (i > 2) || (j < 0) | (j > 2)) {
+      throw new IllegalAccessException("Invalid board position");
+    }
+    if (board[i][j] != EMPTY) {
+      throw new IllegalAccessException("Board position occupied");
+    }
+
+    board[i][j] = player;
+    player = -player;
+  }
+
+  public boolean isWin(int mark) {
+    return ((board[0][0] + board[0][1] + board[0][2] == mark * 3)
+        || (board[1][0] + board[1][1] + board[1][2] == mark * 3)
+        || (board[2][0] + board[2][1] + board[2][2] == mark * 3)
+        || (board[0][0] + board[1][0] + board[2][0] == mark * 3)
+        || (board[0][1] + board[1][1] + board[2][1] == mark * 3)
+        || (board[0][2] + board[1][2] + board[2][2] == mark * 3)
+        || (board[0][0] + board[1][1] + board[2][2] == mark * 3)
+        || (board[2][0] + board[1][1] + board[0][2] == mark * 3));
+  }
+
+  public int winner() {
+    if (isWin(X)) {
+      return X;
+    } else if (isWin(O)) {
+      return O;
+    } else {
+      return 0;
+    }
+  }
+
 }
