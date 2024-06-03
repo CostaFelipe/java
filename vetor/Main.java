da388249f26518176f27989c17780da152021652
wedo <costadefelipe@gmail.com>
2024-06-03 14:27:01 -0300

    delete

--- vetor/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    Vetor vetor = new Vetor(10);
-    vetor.add("teste");
-    vetor.add("teste 1");
-    System.out.println(vetor.display());
-  }
-}
